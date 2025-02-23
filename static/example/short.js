a6d6029bb53154960016dad61031aed457ac00ed
seungheondoh <seungheondoh@kaist.ac.kr>
2025-02-24 01:48:37 +0900

    add more examples

--- static/example/short.js
+++ static/example/short.js
@@ -1,6 +1,249 @@
 const short_0 = {
   "input": [
     {
+      "content": "I am looking for a musical theatre song with powerful vocals, ideally from a well-known musical.",
+      "role": "user"
+    },
+    {
+      "content": "Let's start with a powerful and emotional selection from a popular musical.",
+      "role": "assistant"
+    },
+    {
+      "content": {
+        "track_id": "3Mr6ofzF6ZuYiqHh8qNud3",
+        "title": "On My Own",
+        "artist": "Samantha Barks",
+        "tag_list": [
+          "musical",
+          "les",
+          "misérables",
+          "movie soundtrack",
+          "london",
+          "soundtrack",
+          "unrequited love",
+          "friend zoned",
+          "female vocalists",
+          "perfection",
+          "france",
+          "Soundtracks",
+          "actresses",
+          "actress singer",
+          "tv shows movies characters",
+          " les miserables",
+          "ost: les miserables",
+          "sachen die ich gerne hoeren mag",
+          "Pop"
+        ],
+        "audio_path": "./static/audio/3Mr6ofzF6ZuYiqHh8qNud3.mp3"
+      },
+      "role": "music"
+    },
+    {
+      "content": "That was beautiful, but I want something a bit more upbeat, maybe from a more recent musical.",
+      "role": "user"
+    }
+  ],
+  "prediction": [
+    {
+      "role": "assistant",
+      "content": "Here's an upbeat song from a more recent musical, known for its catchy rhythm."
+    },
+    {
+      "role": "music",
+      "content": {
+        "track_id": "1bpnYrDCforv9ctJMzJRV8",
+        "title": "Defying Gravity - From \"Wicked\" Original Broadway Cast Recording/2003",
+        "artist": "Kristin Chenoweth",
+        "tag_list": [
+          "pop",
+          "musical",
+          "broadway",
+          "Vocal",
+        ],
+        "audio_path": "./static/audio/1bpnYrDCforv9ctJMzJRV8.mp3"
+      }
+    }
+  ],
+  "ground_truth": [
+    {
+      "role": "assistant",
+      "content": "Here's an upbeat selection from a recent hit musical."
+    },
+    {
+      "role": "music",
+      "content": {
+        "track_id": "1yEmO6gdCRzBMch3UHQeyQ",
+        "title": "Seventeen (feat. Barrett Wilbert Weed & Ryan McCartan)",
+        "artist": "Barrett Wilbert Weed",
+        "tag_list": [
+          "Musicals",
+          "Rock",
+          "Soundtracks"
+        ],
+        "audio_path": "./static/audio/1yEmO6gdCRzBMch3UHQeyQ.mp3"
+      }
+    }
+  ]
+};
+const short_1 = {
+  "input": [
+    {
+      "content": "I am in the mood for some country music.  Can you recommend something upbeat and popular?",
+      "role": "user"
+    },
+    {
+      "content": "Sure!  Let's start with \"Singles You Up\" by Jordan Davis. It's a very popular and energetic track.",
+      "role": "assistant"
+    },
+    {
+      "content": {
+        "track_id": "1HehGmky06OZLM9Vz2PRZL",
+        "title": "Singles You Up",
+        "artist": "Jordan Davis",
+        "tag_list": [
+          "Country"
+        ],
+        "audio_path": "./static/audio/1HehGmky06OZLM9Vz2PRZL.mp3"
+      },
+      "role": "music"
+    },
+    {
+      "content": "That was great!  Something similar, but maybe a bit slower and a little more emotional?",
+      "role": "user"
+    }
+  ],
+  "prediction": [
+    {
+      "role": "assistant",
+      "content": "Okay, how about \"Somethin' I'm Good At\" by Brett Eldredge? It has that same country feel, but with a more reflective and emotional tone."
+    },
+    {
+      "role": "music",
+      "content": {
+        "track_id": "6DRsEO5yuEGBF87qpyMB4d",
+        "title": "Somethin' I'm Good At",
+        "artist": "Brett Eldredge",
+        "tag_list": [
+          "2010s",
+          "country",
+          "2017",
+          "Country",
+          "hairy chest"
+        ],
+        "audio_path": "./static/audio/6DRsEO5yuEGBF87qpyMB4d.mp3"
+      }
+    }
+  ],
+  "ground_truth": [
+    {
+      "role": "assistant",
+      "content": "Okay, how about \"Blue Tacoma\" by Russell Dickerson? It's still country, but a bit more mellow."
+    },
+    {
+      "role": "music",
+      "content": {
+        "track_id": "2oPCR02nduYVpj7CTfsA0d",
+        "title": "Blue Tacoma",
+        "artist": "Russell Dickerson",
+        "tag_list": [
+          "Country"
+        ],
+        "audio_path": "./static/audio/2oPCR02nduYVpj7CTfsA0d.mp3"
+      }
+    }
+  ]
+};
+const short_2 = {
+  "input": [
+    {
+      "content": "I want some upbeat Latin music to get the party started!",
+      "role": "user"
+    },
+    {
+      "content": "Let's start with a popular and energetic track. This one is known for its high energy and party atmosphere.",
+      "role": "assistant"
+    },
+    {
+      "content": {
+        "track_id": "4pg1CRAajCYbb1q2sZypPf",
+        "title": "Echa Pa'lla (Manos Pa'rriba)",
+        "artist": "Pitbull",
+        "tag_list": [
+          "Latin"
+        ],
+        "audio_path": "./static/audio/4pg1CRAajCYbb1q2sZypPf.mp3"
+      },
+      "role": "music"
+    },
+    {
+      "content": "Great! Now, something a bit more traditional, maybe with a Cuban salsa feel?",
+      "role": "user"
+    }
+  ],
+  "prediction": [
+    {
+      "role": "assistant",
+      "content": "Here's a classic Cuban salsa track that captures the energy and impact of traditional sounds."
+    },
+    {
+      "role": "music",
+      "content": {
+        "track_id": "2AR6gN4QVBdJ8iw2KSG7fQ",
+        "title": "Es Mentiroso",
+        "artist": "Olga Tanon",
+        "tag_list": [
+          "latino",
+          "casandra",
+          "mariya",
+          "female vocalist",
+          "elvrother",
+          "yo soy el vrother",
+          "natalia",
+          "el vrother",
+          "Latin",
+          "frankgardy 28",
+          "eres",
+          "salsa",
+          "merengue",
+          "love songs",
+          "female vocalists",
+          "Latin Pop",
+          "soy tu bachatu",
+          "latin",
+          "nayer",
+          "olga tanon mentiroso"
+        ],
+        "audio_path": "./static/audio/2AR6gN4QVBdJ8iw2KSG7fQ.mp3"
+      }
+    }
+  ],
+  "ground_truth": [
+    {
+      "role": "assistant",
+      "content": "Here's a classic Cuban salsa track, known for its authentic sound and infectious rhythm."
+    },
+    {
+      "role": "music",
+      "content": {
+        "track_id": "6j9DqgqLwLFSEmK4gWs7NY",
+        "title": "La borrachera",
+        "artist": "Pupy y Los Que Son Son",
+        "tag_list": [
+          "salsa cubana",
+          "salsa",
+          "Jazz",
+          "latin",
+          "cuba",
+          "timba"
+        ],
+        "audio_path": "./static/audio/6j9DqgqLwLFSEmK4gWs7NY.mp3"
+      }
+    }
+  ]
+};
+const short_3 = {
+  "input": [
+    {
       "content": "I'm in the mood for some classic rock, something with a strong guitar riff and a bit of a psychedelic vibe.",
       "role": "user"
     },
@@ -70,7 +313,7 @@ const short_0 = {
     }
   ]
 };
-const short_1 = {
+const short_4 = {
   "input": [
     {
       "content": "I'm in the mood for some upbeat pop music with strong vocals, released after 2010.",
@@ -155,7 +398,7 @@ const short_1 = {
     }
   ]
 };
-const short_2 = {
+const short_5 = {
   "input": [
     {
       "content": "I'm looking for some upbeat alternative rock songs to get me energized.",
